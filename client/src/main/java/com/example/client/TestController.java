49d84efb36648d2d1fb9446146879624c07ecf4c
dlsrb6342 <dlsrb6342@gmail.com>
2019-07-29 20:24:41 +0900

    Initial commit

---
+++ client/src/main/java/com/example/client/TestController.java
@@ -0,0 +1,40 @@
+package com.example.client;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.BulkheadRegistry;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryRegistry;
+
+@RestController
+public class TestController {
+
+    private final CircuitBreakerRegistry circuitBreakerRegistry;
+    private final RetryRegistry retryRegistry;
+    private final RateLimiterRegistry rateLimiterRegistry;
+    private final BulkheadRegistry bulkheadRegistry;
+
+    public TestController(CircuitBreakerRegistry circuitBreakerRegistry,
+                          RetryRegistry retryRegistry,
+                          RateLimiterRegistry rateLimiterRegistry,
+                          BulkheadRegistry bulkheadRegistry) {
+        this.circuitBreakerRegistry = circuitBreakerRegistry;
+        this.retryRegistry = retryRegistry;
+        this.rateLimiterRegistry = rateLimiterRegistry;
+        this.bulkheadRegistry = bulkheadRegistry;
+    }
+
+    @GetMapping("/test")
+    public void test() {
+        CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker("test");
+        Retry retry = retryRegistry.retry("test");
+        RateLimiter rateLimiter = rateLimiterRegistry.rateLimiter("test");
+        Bulkhead bulkhead = bulkheadRegistry.bulkhead("test");
+    }
+}
---
+++ noclient/src/main/java/com/example/noclient/NoclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.noclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoclientApplication.class, args);
+    }
+
+}
